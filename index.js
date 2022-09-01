4e67a3a6a0daa02ecc3d55f898ffd34ce245a101
lee mi ji <dlalwl223@naver.com>
2022-09-01 15:54:12 +0900

    update

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.delete("/voca/:id", (req, res) => {
 app.put("/voca/:id", (req, res) => {
   const _id = parseInt(req.params.id);
   const _isDone = Boolean(req.body.isDone);
-  console.log(_isDone);
+  console.log(_id);
   db.collection("vocas").updateOne({ id: _id }, { $set: { isDone: _isDone } }, (err, result) => {
     res.json({ update: "ok" });
   });
